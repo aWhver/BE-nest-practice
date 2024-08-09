3ec8c8070a256f54bab393e2c36ed183818bda70
zhaojuntong <13714040198@163.com>
2024-08-09 11:44:57 +0800

    feat: sse

--- nest-basic/src/app.controller.ts
+++ nest-basic/src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query, Sse } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Observable } from 'rxjs';
+import { exec } from 'child_process';
 
 @Controller()
 export class AppController {
@@ -8,5 +10,22 @@ export class AppController {
   @Get('sayhi')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Sse('stream/:user')
+  stream(@Param('user') user: string, @Query('msgType') msgType: string) {
+    console.log('user, msgType', user, msgType);
+    const childProcess = exec('tail -f ./src/sse.txt');
+
+    return new Observable((observe) => {
+      observe.next('立即推送的消息');
+      childProcess.stdout.on('data', (msg) => {
+        console.log('msg', msg);
+        observe.next(msg);
+      });
+      // setTimeout(() => {
+      //   observe.next('延时3s的消息');
+      // }, 3000);
+    });
   }
 }
